09d4f2a749bb5d4c42218fe0524c9fb4f31ae3fb
Your Name <you@example.com>
2014-03-16 12:43:28 +0800

    add Client

---
+++ Client/Client/deal.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Client {
+  partial class deal {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.lbl_title = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lbl_title
+      // 
+      this.lbl_title.AutoSize = true;
+      this.lbl_title.Location = new System.Drawing.Point(70, 26);
+      this.lbl_title.Name = "lbl_title";
+      this.lbl_title.Size = new System.Drawing.Size(127, 13);
+      this.lbl_title.TabIndex = 0;
+      this.lbl_title.Text = "欢迎使用联机交易网关";
+      // 
+      // deal
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(289, 386);
+      this.Controls.Add(this.lbl_title);
+      this.Name = "deal";
+      this.Text = "deal";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lbl_title;
+  }
+}
---
+++ Client/Client/keyconfig.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+  public class keyconfig
+  {
+    private static byte[] physics = new byte[20];
+    private static byte[] flow = new byte[20];
+    private static int machineid = 0;
+    public keyconfig()
+    {
+      if (machineid == 0)
+      {
+        generatepysics();
+        generateflow();
+      }
+    }
+
+    public keyconfig(string fl)
+    {
+      if (machineid == 0)
+      {
+        generatepysics();
+        generateflow(fl);
+      }
+    }
+
+    public keyconfig(string py, string fl)
+    {
+      if (machineid == 0)
+      {
+        generatepysics(py);
+        generateflow(fl);
+      }
+    }
+
+    private void generatepysics()
+    {
+      string sub_physics = ConfigurationSettings.AppSettings["physics"];
+      char[] tmp = sub_physics.ToCharArray();
+      for (int i = 0; i < tmp.Length; i++) {
+        physics[i] = Convert.ToByte(tmp[i]);
+      }
+      if (sub_physics.Length < 20) {
+        for (int i = tmp.Length ; i < physics.Length; i++) {
+          physics[i] = Convert.ToByte('#');
+        }
+      }
+    }
+
+    private void generatepysics(string py)
+    {
+      char [] tmp = py.ToCharArray();
+      for (int i = 0; i < tmp.Length; i++)
+      {
+        physics[i] = Convert.ToByte(tmp[i]);
+      }
+      if (py.Length < 20)
+      {
+        for (int i = tmp.Length - 1; i < physics.Length; i++)
+        {
+          physics[i] = Convert.ToByte('#');
+        }
+      }
+    }
+
+    private void generateflow()
+    {
+      Random rnd = new Random();
+      for (int i = 0; i < flow.Length; i++)
+      {
+        flow[i] = Convert.ToByte(Convert.ToChar(rnd.Next(9)));
+      }
+    }
+
+    private void generateflow(string fl) {
+      char[] tmp = fl.ToCharArray();
+      for (int i = 0; i < tmp.Length; i++) {
+        flow[i] = Convert.ToByte(tmp[i]);
+      }
+      if (fl.Length < 20) {
+        for (int i = tmp.Length; i < physics.Length; i++) {
+          flow[i] = Convert.ToByte('#');
+        }
+      }
+    }
+
+    public byte[] Physics
+    {
+      get { return physics; }
+      set { physics = value; }
+    }
+
+    public byte[] Flow
+    {
+      get { return flow; }
+      set { flow = value; }
+    }
+
+    public int Machineid
+    {
+      get { return machineid; }
+      set { machineid = value; }
+    }
+
+    public static byte[] encryption(byte[] message, byte[] key)
+    {
+      if (key.Length < 16)
+      {
+        throw new Exception("Key shoud more than 16 bytes.");
+      }
+      for (int i = 0, j = 0; i < message.Length; i++, j++) {
+        if (i % 16 == 0) {
+          j = 0;
+        }
+        message[i] += key[j];
+      }
+      return message;
+    }
+
+    public static byte[] decryption(byte[] message, byte[] key)
+    {
+      if (key.Length < 16) {
+        throw new Exception("Key shoud more than 16 bytes.");
+      }
+      for (int i = 0, j = 0; i < message.Length; i++, j++) {
+        if (i%16 == 0)
+        {
+          j = 0;
+        }
+         message[i] -= key[j];
+      }
+      return message;
+    }
+  }
+}
